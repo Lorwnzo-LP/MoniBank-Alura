75c892723a5793c671d11ce005be695ff159119c
Lorenzo Loss <lorenzocontato1@outlook.com>
2024-12-07 14:35:01 -0300

    commit de avanço

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
+import UmCPF from "./valida-cpf.js";
+
 const camposDoFormulario = document.querySelectorAll("[required]");
 
 camposDoFormulario.forEach((campo) => {
-    campo.addEventListener("blur", () => verificaCampo())
+    campo.addEventListener("blur", () => verificaCampo(campo))
 }) 
+
+function verificaCampo(campo) {
+    if (campo.name == "cpf" && campo.value.length >= 11){
+        UmCPF(campo);
+    }
+}
