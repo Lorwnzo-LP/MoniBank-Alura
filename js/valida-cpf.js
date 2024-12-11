458aba51a117e8d8aeb7797c66c376e129258714
Lorenzo Loss <lorenzocontato1@outlook.com>
2024-12-11 00:05:06 -0300

    Atualização dos itens

--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -1,6 +1,8 @@
-export default function UmCPF(campo){
-    const cpf = campo.value.replace(/\.|-/g,"");
-    validaNumerosRepetidos(cpf);
+export default function UmCPF(campo) {
+    const cpf = campo.value.replace(/\.|-/g, "");
+    if (validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)) {
+        campo.setCustomValidity('Esse cpf não é válido');
+    }
 }
 
 function validaNumerosRepetidos(cpf){
@@ -15,7 +17,41 @@ function validaNumerosRepetidos(cpf){
         '77777777777',
         '88888888888',
         '99999999999'
-        ]
+    ]
 
-        numerosRepetidos.includes(cpf);
+    return numerosRepetidos.includes(cpf);
+}
+
+function validaPrimeiroDigito(cpf){
+    let soma = 0;
+    let multiplicador = 10;
+
+    for(let tamanho = 0; tamanho < 9; tamanho++){
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+    soma = (soma*10) % 11;
+    if (soma == 10 || soma == 11){
+        soma = 0;
+    }
+
+    return soma != cpf[9]
+}
+
+function validaSegundoDigito(cpf){
+    let soma = 0;
+    let multiplicador = 11;
+
+    for(let tamanho = 0; tamanho < 10; tamanho++){
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+
+    soma = (soma * 10) % 11;
+    
+    if (soma == 10 || soma == 11){
+        soma = 0;
+    }
+
+    return soma != cpf[10]
 }
